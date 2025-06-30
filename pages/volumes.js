b8cc18208753301539c1803381984db87a310310
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-06-30 12:17:27 +0200

    fixes

--- pages/volumes.js
+++ pages/volumes.js
@@ -1,7 +1,22 @@
 import Link from "next/link";
 import { volumes } from "../lib/data";
+import { useRouter } from "next/router";
+//useRouter() lets us navigate programmatically instead of using <Link>
+
+function getRandomSlug(array) {
+  // const randomNum = Math.floor(Math.random() * array.length)
+  // return array[randomNum]
+  return array[Math.floor(Math.random() * array.length)].slug;
+}
+//picks a random item from any array
 
 export default function Volumes() {
+  const router = useRouter();
+
+  function handleRandom() {
+    const randomVol = getRandomSlug(volumes);
+    router.push(`/volumes/${randomVol}`);
+  }
   return (
     <div>
       <h1>All Volumes</h1>
@@ -13,6 +28,7 @@ export default function Volumes() {
           </li>
         ))}
       </ul>
+      <button onClick={handleRandom}>Go to a Random Volume</button>
     </div>
   );
 }
